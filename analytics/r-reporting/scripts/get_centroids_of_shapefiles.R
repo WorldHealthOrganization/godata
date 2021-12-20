7ee61e3926822ba71d6f2cc8f361e136aec0f49a
Sara Hollis <57128361+sarahollis@users.noreply.github.com>
2021-12-20 16:41:48 +0100

    Create get_centroids_of_shapefiles.R

---
+++ analytics/r-reporting/scripts/get_centroids_of_shapefiles.R
@@ -0,0 +1,26 @@
+# script to get centroids of shapefiles, as Go.Data only ingests points and not polygons
+
+# --------------
+library(raster)
+library(rgdal)
+library(readxl)
+​
+#------------------ setting path
+path = "yourfilepath"
+setwd(path)
+​
+dsn1=paste0(path,"Shape_files/gadm36_SEN_shp")
+SEN <- readOGR(dsn = dsn1, layer = "gadm36_SEN_4", verbose = FALSE)
+plot(SEN)
+​
+##### recuperer les centroids pour les lables ,
+centroids <- as.data.frame(coordinates(SEN))
+names(centroids) <- c("Longitude", "Latitude") 
+centroids$Nom_arron = SEN$NAME_3
+centroids$Code_arron = SEN$GID_3
+centroids$Nom_communes <- SEN$NAME_4
+centroids$Code_communes <- SEN$GID_4
+​
+centroids <- as.data.frame(centroids)
+​
+write.csv2(centroids,"/Users/maryam/Downloads/Go_Data_23_11_2021/centroids_communes_sn.csv", row.names = FALSE)
